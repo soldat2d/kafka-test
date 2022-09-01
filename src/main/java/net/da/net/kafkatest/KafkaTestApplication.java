e39a393fe42925df8fa082b51d633d647c0fa2d8
younguru <kiosun@mail.ru>
2022-09-01 16:58:56 +0300

    3

--- src/main/java/net/da/net/kafkatest/KafkaTestApplication.java
+++ src/main/java/net/da/net/kafkatest/KafkaTestApplication.java
@@ -6,36 +6,53 @@ import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.Closeable;
-import java.io.IOException;
 import java.time.Duration;
+import java.util.Collections;
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 import java.util.stream.IntStream;
 
 @SpringBootApplication
 public class KafkaTestApplication {
 
-    public static void main(String[] args) throws ExecutionException, InterruptedException, IOException {
-//        SpringApplication.run(KafkaTestApplication.class, args);
+    public static void main(String[] args) throws InterruptedException {
+        SpringApplication.run(KafkaTestApplication.class, args);
         var topic = "kafka-test";
+
         var producer = new MyProducer(topic);
-        IntStream.range(0, 100).forEach( i ->
-                producer.send(i.toString));
+        new Thread(() -> {
+            IntStream.range(0, 10).forEach(i -> {
+                try {
+                    System.out.println("SENDING !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+                    producer.send(Integer.toString(i), "producer send " + i);
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (ExecutionException | InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        }).start();
 
+        var consumer = new MyConsumer(topic);
+        consumer.consume(record -> System.out.println("key: " + record.key() + "  value: " + record.value()));
+
+        TimeUnit.SECONDS.sleep(500);
         producer.close();
+        consumer.close();
     }
-
 }
 
 class MyConsumer implements Closeable {
 
-    private String topic;
-    private KafkaConsumer<String, String> consumer;
+    private final String topic;
+    private final KafkaConsumer<String, String> consumer;
 
     MyConsumer(String topic) {
         this.topic = topic;
@@ -45,31 +62,33 @@ class MyConsumer implements Closeable {
     private KafkaConsumer<String, String> getConsumer() {
         var props = new Properties();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "172.18.249.202:9092");
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "clientId");
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        return new KafkaConsumer(props);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "groupId");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        var consumer = new KafkaConsumer<String, String>(props);
+        consumer.subscribe(Collections.singletonList(topic));
+        return consumer;
     }
 
     public void consume(Consumer<ConsumerRecord<String, String>> recordConsumer) {
         new Thread(() -> {
             while (true) {
                 var records = consumer.poll(Duration.ofSeconds(1));
-                records.forEach(record -> recordConsumer.accept(record));
+                records.forEach(recordConsumer);
             }
         }).start();
     }
 
     @Override
-    public void close() throws IOException {
+    public void close() {
         consumer.close();
     }
 }
 
 class MyProducer implements Closeable {
 
-    private String topic;
-    private KafkaProducer<String, String> producer;
+    private final String topic;
+    private final KafkaProducer<String, String> producer;
 
     MyProducer(String topic) {
         this.topic = topic;
@@ -82,7 +101,7 @@ class MyProducer implements Closeable {
         props.put(ProducerConfig.CLIENT_ID_CONFIG, "clientId");
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        return new KafkaProducer(props);
+        return new KafkaProducer<>(props);
     }
 
     public void send(String key, String value) throws ExecutionException, InterruptedException {
@@ -90,7 +109,7 @@ class MyProducer implements Closeable {
     }
 
     @Override
-    public void close() throws IOException {
+    public void close() {
         producer.close();
     }
 }
